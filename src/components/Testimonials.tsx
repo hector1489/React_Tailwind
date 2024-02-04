ea17bcb039c88cb140b3856a14cbfe84ce15a98f
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-02-04 11:30:51 -0300

    [fix] structuring

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,9 +2,20 @@
 const Testimonials = () => {
 
 
-    return (
-        <p>Testimonials</p>
-    )
+  return (
+    <div className="card card-side bg-base-100 shadow-xl">
+        <div className="w-24 rounded-full">
+          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      <div className="card-body">
+        <h2 className="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Testimonials
