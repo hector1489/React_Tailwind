88f745c9dd941e1515bf8f5fe73fb4aae453329a
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-02-03 22:17:58 -0300

    [fix] import css

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import NavBar from './components/NavBar'
 import Home from './views/Home'
 import './App.css'
 
-function App() {
-
-
+const App: React.FC = () => {
   return (
-    <>
+    <div className="app-container">
       <NavBar />
       <Home />
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/daisyui/dist/**/*.js",
-    "node_modules/react-daisyui/dist/**/*.js",
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
 ],
   theme: {
     extend: {},
