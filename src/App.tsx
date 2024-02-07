2aeef0873019fbb13a091192f812b915f95a94d9
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-02-07 09:33:01 -0300

    [fix] routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css'
 import NavBar from './components/NavBar'
 import Home from './views/Home'
-import './App.css'
+import MainGallery from "./components/MainGallery"
+import Testimonials from "./components/Testimonials"
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div className="app-container">
+    <BrowserRouter>
       <NavBar />
-      <Home />
-    </div>
-  );
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="gallery" element={<MainGallery />} />
+        <Route path="testimonials" element={<Testimonials />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
